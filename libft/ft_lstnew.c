22acc57861517494b54ac296034b4f6e9b27bc3a
Maxime Echard <mechard@bess-f2r6s5.clusters.42paris.fr>
2023-11-17 14:50:40 +0100

    /home/mechard/Documents/42_Paris/42_Paris_2023/Cursus_2023 - update of Fri Nov 17 02:50:40 PM CET 2023

---
+++ libft/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mechard <mechard@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/10 15:12:32 by mechard           #+#    #+#             */
+/*   Updated: 2023/11/15 14:30:17 by mechard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*lst;
+
+	lst = (t_list *)malloc(sizeof(t_list));
+	if (!lst)
+		return (NULL);
+	lst->content = content;
+	lst->next = NULL;
+	return (lst);
+}
