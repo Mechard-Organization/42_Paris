3788743245adefe53a4901df2ef58ef7adc57e73
Maxime Echard <mechard@paul-f4Ar6s1.clusters.42paris.fr>
2023-12-05 17:39:19 +0100

    /home/mechard/Documents/42_Paris/42_Paris_2023 - update of Tue Dec  5 05:39:19 PM CET 2023

--- 42_Paris_2023/Cursus_2023/ft_printf/ft_printf.c
+++ 42_Paris_2023/Cursus_2023/ft_printf/ft_printf.c
@@ -6,61 +6,76 @@
 /*   By: mechard <mechard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 14:31:17 by mechard           #+#    #+#             */
-/*   Updated: 2023/12/01 16:25:06 by mechard          ###   ########.fr       */
+/*   Updated: 2023/12/05 17:30:18 by mechard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libftprintf.h"
+#include "ft_printf.h"
 
-int	ft_display(const char *str, va_list arg)
+int	ft_check(char *str, int i, va_list args, int *len)
 {
-	int		len;
-	int		tmp;
-	t_res	res;
-	char	*dis;
+	char	*flags;
+	char	*bonus;
+	int		fm;
+	int		fb;
 
-	len = 0;
-	dis = NULL;
-	res.len = 0;
-	while (str[len])
+	fm = 0;
+	bonus = "# +";
+	flags = "cdipPsuxX%";
+	while (flags[fm])
 	{
-		if (str[len] != '%')
-			ft_putchar_fd(str[len], 1);
-		else
-		{
-			tmp = res.len;
-			printf("\ntmp avant = %d\n", (int)tmp);
-			res.len = 0;
-			res = ft_parse(str, len, arg);
-			if (!res.res)
-				return (ft_putstr_fd("\nErreur d'arguments !\n", 1), 22);
-			len += res.len_f;
-			printf("\nres.len avant = %d\n", res.len);
-			res.len += tmp + ft_strlen(res.res);
-			printf("\nres.len apres = %d\n", res.len);
-			ft_putstr_fd(res.res, 1);
-		}
-		res.len++;
-		len++;
+		if (str[i] == flags[fm])
+			return (ft_check_mand(str + i, args, len) + i);
+		fm++;
 	}
-	if (res.len_f == 1)
-		res.len -= 2;
-	res.len--;
-	// printf("\nres.len_f = %d\n", res.len_f);
-	// printf("res.len = %d\n\n", res.len);
-	// printf("len = %d\n\n", len);
-	// ft_putstr_fd(dis, 1);
-	return (res.len);
+	fb = 0;
+	while (bonus[fb])
+	{
+		if (str[i] == bonus[fb] || ft_isdigit(str[i]) == 1)
+			return (ft_check_bonu(str + i, args, len) + i);
+		fb++;
+	}
+	return (0);
 }
 
-int	ft_printf(const char *form, ...)
+int	ft_parse(char *str, va_list args, int *len)
 {
-	va_list	arg;
-	int		i;
+	int	nb_space;
+	int	i;
 
+	i = 1;
+	nb_space = 0;
+	if (ft_isdigit(str[i]) == 1)
+		nb_space = ft_atoi(str + i);
+	if (nb_space > 0)
+		i += ft_nbrlen(nb_space, 10);
+	while (nb_space > 0)
+	{
+		ft_putchar(' ', len);
+		nb_space--;
+	}
+	return (ft_check(str, i, args, len));
+}
+
+int	ft_printf(const char *s, ...)
+{
+	size_t	i;
+	va_list	args;
+	char	*str;
+	int		len;
+
+	str = ft_strdup(s);
+	va_start(args, s);
+	len = 0;
 	i = 0;
-	va_start(arg, form);
-	i = ft_display(form, arg);
-	va_end(arg);
-	return (i);
+	while (str[i])
+	{
+		if (str[i] == '%')
+			i += ft_parse(str + i, args, &len);
+		else
+			ft_putchar(str[i], &len);
+		i++;
+	}
+	free(str);
+	return (len);
 }
