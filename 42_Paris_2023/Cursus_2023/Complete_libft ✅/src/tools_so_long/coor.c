187043e4f4d06461bc115af66ac19cb76ba36591
HardDude <mechard@harddude.42>
2024-07-08 15:36:28 +0200

    /home/mechard/Documents/42_paris_2/42_Paris_2023/Cursus_2023/Complete_libft ✅ - update of Mon Jul  8 03:36:28 PM CEST 2024

--- 42_Paris_2023/Cursus_2023/Complete_libft ✅/src/tools_so_long/coor.c
+++ 42_Paris_2023/Cursus_2023/Complete_libft ✅/src/tools_so_long/coor.c
@@ -1,37 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push.c                                             :+:      :+:    :+:   */
+/*   coor.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mechard <mechard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/26 18:41:31 by mechard           #+#    #+#             */
-/*   Updated: 2024/03/26 18:41:52 by mechard          ###   ########.fr       */
+/*   Created: 2024/06/11 09:47:35 by mechard           #+#    #+#             */
+/*   Updated: 2024/07/08 13:17:35 by mechard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#include "libft.h"
 
-static void	push(t_stack **stack_a, t_stack **stack_b)
+void	ft_coor(int x, int y, t_map *coor)
 {
-	t_stack	*tmp;
-
-	if (!*stack_b)
-		return ;
-	tmp = *stack_a;
-	*stack_a = *stack_b;
-	*stack_b = (*stack_b)->next;
-	(*stack_a)->next = tmp;
-}
-
-void	pa(t_stack **stack_a, t_stack **stack_b)
-{
-	push(stack_a, stack_b);
-	ft_printf("pa\n");
-}
-
-void	pb(t_stack **stack_a, t_stack **stack_b)
-{
-	push(stack_b, stack_a);
-	ft_printf("pb\n");
+	coor->player_x = x;
+	coor->player_y = y;
+	coor->nb_p++;
 }
