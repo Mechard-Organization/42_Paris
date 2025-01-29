1d559080e97024133af5a234bfa6bd1f18dc839d
Maxime Echard <mechard@bess-f2r5s13.clusters.42paris.fr>
2025-01-29 10:37:34 +0100

    Squashed 'Projects/In_progress/philo/' content from commit 694bc1e
    git-subtree-dir: Projects/In_progress/philo
    git-subtree-split: 694bc1e0d961b50c818484fa48ac9e06837a13b0

---
+++ philo/ft_str.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mechard <mechard@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/17 15:02:35 by mechard           #+#    #+#             */
+/*   Updated: 2024/12/12 14:36:05 by mechard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (s)
+		write(fd, s, ft_strlen(s));
+}
+
+void	ft_putnbr_fd(ssize_t n, int fd)
+{
+	size_t	nb;
+
+	if (n < 0)
+	{
+		nb = -n;
+		ft_putchar_fd('-', fd);
+	}
+	else
+		nb = n;
+	if (nb >= 10)
+	{
+		ft_putnbr_fd((nb / 10), fd);
+		ft_putchar_fd(((nb % 10) + '0'), fd);
+	}
+	else
+	{
+		if (n > 0)
+			ft_putchar_fd(('0' + n), fd);
+		else
+			ft_putchar_fd(('0' - n), fd);
+	}
+}
