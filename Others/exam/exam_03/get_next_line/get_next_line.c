9d0d7476b7fdc7a2f5c713d6623c4520e432e986
M1000 <143119729+M1000-93@users.noreply.github.com>
2025-02-03 21:01:48 +0100

    Update get_next_line.c

--- Others/exam/exam_03/get_next_line/get_next_line.c
+++ Others/exam/exam_03/get_next_line/get_next_line.c
@@ -1,31 +1,69 @@
 #include <stdlib.h>
 #include <unistd.h>
+#include "get_next_line.h"
 
-char	*get_next_line(int fd)
+char    *ft_strdup(char *src)
 {
-	char	*res;
-	char	buffer[999999] = {0};
-	char	character;
-	int		i;
+    char    *dst;
+    int        i;
 
-	i = 0;
-	if (fd < 0)
-		return (NULL);
-	while ((read(fd, &character, 1)) > 0)
-	{
-		buffer[i++] = character;
-		if (character == '\n')
-			break ;
-	}
-	if (!buffer[0])
-		return (NULL);
-	res = malloc(i + 1);
-	i = 0;
-	while (buffer[i])
-	{
-		res[i] = buffer[i];
-		i++;
-	}
-	res[i] = '\0';
-	return (res);
+    i = 0;
+    while (src[i])
+        i++;
+    dst = malloc(sizeof(char) * (i + 1));
+    i = -1;
+    while (src[++i])
+        dst[i] = src[i];
+    dst[i] = '\0';
+    return (dst);
+}
+
+char    *get_next_line(int fd)
+{
+    static char buffer[BUFFER_SIZE];
+    char        line[700000];
+    static    int     buffer_reading;
+    static    int     buffer_pos;
+    int                i;
+
+    i = 0;
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (NULL);
+    
+    while (1)
+    {
+        if (buffer_pos >= buffer_reading)
+        {
+            buffer_reading = read(fd, buffer, BUFFER_SIZE);
+            buffer_pos = 0;
+            if (buffer_reading <= 0)
+                break;
+        }
+        line[i++] = buffer[buffer_pos++];
+        if (line[i] == '\n')
+            break ;
+    }
+    line[i] = '\0';
+    if (i == 0)
+        return (NULL);
+    return (ft_strdup(line));
+}
+
+#include <fcntl.h>
+#include <stdio.h>
+
+int    main(void)
+{
+    char    *line;
+    int        fd;
+
+    fd = open("txt.txt", O_RDONLY);
+    while (1)
+    {
+        line = get_next_line(fd);
+        if (line == NULL)
+            break ;
+        printf("%s", line);
+        free(line);
+    }
 }
