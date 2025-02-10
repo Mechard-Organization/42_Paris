3425f5e5ce48e243aaeca3635fe138044c03734d
M1000 <143119729+M1000-93@users.noreply.github.com>
2025-02-10 19:01:39 +0100

    Update get_next_line.h

--- Others/exam/exam_03/get_next_line/get_next_line.h
+++ Others/exam/exam_03/get_next_line/get_next_line.h
@@ -1,13 +1,11 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <fcntl.h>
-# include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1024
-# endif
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 32
+#endif
 
 #endif
