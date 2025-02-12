ed01a691b3804f952a340c5958f889d575148a76
Maxime Echard <mechard@bess-f2r3s16.clusters.42paris.fr>
2025-02-12 14:59:22 +0100

    Wed Feb 12 02:59:22 PM CET 2025 - /home/mechard/Desktop/42_Paris/42_Paris_2023/Cursus_2023/Projects/In_progress/cpp_00/ex00 update

--- 42_Paris_2023/Cursus_2023/Projects/In_progress/cpp_00/ex00/megaphone.cpp
+++ 42_Paris_2023/Cursus_2023/Projects/In_progress/cpp_00/ex00/megaphone.cpp
@@ -1,12 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Megaphone.cpp                                      :+:      :+:    :+:   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mechard <mechard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 14:44:00 by mechard           #+#    #+#             */
-/*   Updated: 2025/02/12 14:45:18 by mechard          ###   ########.fr       */
+/*   Updated: 2025/02/12 14:57:17 by mechard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <cctype>
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		while (++i < ac)
+		{
+			j = 0;
+			while (av[i][j])
+				std::cout << (char)std::toupper(av[i][j++]);
+			std::cout << " ";
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
