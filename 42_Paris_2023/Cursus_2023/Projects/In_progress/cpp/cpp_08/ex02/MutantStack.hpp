4b6c58f2bf55d2044ecaf85478322475c44a0bb0
Noralia <sarahgoigoux@gmail.com>
2025-03-27 09:26:34 +0800

    push of /home/noralia/maxime/Desktop/42_Paris/42_Paris_2023/Cursus_2023/Projects/In_progress/cpp/cpp_08 - update of Thu Mar 27 09:26:34 CST 2025

---
+++ 42_Paris_2023/Cursus_2023/Projects/In_progress/cpp/cpp_08/ex02/MutantStack.hpp
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mechard <mechard@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/27 09:10:55 by mechard           #+#    #+#             */
+/*   Updated: 2025/03/27 09:10:55 by mechard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <stack>
+#include <deque>
+#include <iostream>
+
+/* Codes ANSI pour la couleur du texte */
+// Réinitialisation
+#define RESET          "\033[0m"
+
+// Couleurs standards du texte
+#define BLACK1          "\033[0;30m"
+#define RED1            "\033[0;31m"
+#define GREEN1          "\033[0;32m"
+#define YELLOW1         "\033[0;33m"
+#define BLUE1           "\033[0;34m"
+#define MAGENTA1        "\033[0;35m"
+#define CYAN1           "\033[0;36m"
+#define WHITE1          "\033[0;37m"
+
+// Couleurs en gras (couleurs claires)
+#define BLACK2     "\033[1;30m"
+#define RED2       "\033[1;31m"
+#define GREEN2     "\033[1;32m"
+#define YELLOW2    "\033[1;33m"
+#define BLUE2      "\033[1;34m"
+#define MAGENTA2   "\033[1;35m"
+#define CYAN2      "\033[1;36m"
+#define WHITE2     "\033[1;37m"
+
+template <typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container>
+{
+
+public:
+    MutantStack();
+    MutantStack(const MutantStack &other);
+    MutantStack &operator=(const MutantStack &other);
+    ~MutantStack();
+    typedef typename Container::iterator iterator;
+    typedef typename Container::const_iterator const_iterator;
+    typedef typename Container::reverse_iterator reverse_iterator;
+    typedef typename Container::const_reverse_iterator const_reverse_iterator;
+    iterator begin();
+    iterator end();
+    const_iterator begin() const;
+    const_iterator end() const;
+    reverse_iterator rbegin();
+    reverse_iterator rend();
+    const_reverse_iterator rbegin() const;
+    const_reverse_iterator rend() const;
+
+};
+
+#include "MutantStack.tpp"
+
+#endif
---
+++ 42_Paris_2023/Cursus_2023/Projects/In_progress/cpp/cpp_08/ex02/main.cpp
@@ -0,0 +1,116 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mechard <mechard@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/27 09:12:13 by mechard           #+#    #+#             */
+/*   Updated: 2025/03/27 09:12:13 by mechard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+
+int main()
+{
+    // ---------------- Test 1 : Push, Top, Pop, Size ----------------
+    std::cout << YELLOW2 << "----------------Test 1----------------" << RESET << std::endl;
+    try {
+        MutantStack<int> ms;
+        ms.push(10);
+        ms.push(20);
+        ms.push(30);
+        std::cout << GREEN2 << "Top element: " << BLUE2 << ms.top() << RESET << std::endl; // Attendu : 30
+        ms.pop();
+        std::cout << GREEN2 << "Size after pop: " << BLUE2 << ms.size() << RESET << std::endl; // Attendu : 2
+    }
+    catch (std::exception &e) {
+        std::cout << RED2 << "Erreur: " << RESET << e.what() << std::endl;
+    }
+    
+    // ---------------- Test 2 : Itération (begin/end) ----------------
+    std::cout << YELLOW2 << "\n----------------Test 2----------------" << RESET << std::endl;
+    try {
+        MutantStack<int> ms;
+        ms.push(5);
+        ms.push(15);
+        ms.push(25);
+        ms.push(35);
+        std::cout << GREEN2 << "Contenu de la MutantStack: " << RESET;
+        for (MutantStack<int>::iterator it = ms.begin(); it != ms.end(); ++it)
+            std::cout << BLUE2 << *it << RESET << " ";
+        std::cout << std::endl;
+    }
+    catch (std::exception &e) {
+        std::cout << RED2 << "Erreur: " << RESET << e.what() << std::endl;
+    }
+    
+    // ---------------- Test 3 : Itération inverse (rbegin/rend) ----------------
+    std::cout << YELLOW2 << "\n----------------Test 3----------------" << RESET << std::endl;
+    try {
+        MutantStack<int> ms;
+        ms.push(100);
+        ms.push(200);
+        ms.push(300);
+        ms.push(400);
+        std::cout << GREEN2 << "Contenu inversé de la MutantStack: " << RESET;
+        for (MutantStack<int>::reverse_iterator rit = ms.rbegin(); rit != ms.rend(); ++rit)
+            std::cout << BLUE2 << *rit << RESET << " ";
+        std::cout << std::endl;
+    }
+    catch (std::exception &e) {
+        std::cout << RED2 << "Erreur: " << RESET << e.what() << std::endl;
+    }
+    
+    // ---------------- Test 4 : Constructeur par copie ----------------
+    std::cout << YELLOW2 << "\n----------------Test 4----------------" << RESET << std::endl;
+    try {
+        MutantStack<int> ms;
+        ms.push(1);
+        ms.push(2);
+        ms.push(3);
+        MutantStack<int> msCopy(ms); // Copie via le constructeur par copie
+        std::cout << GREEN2 << "Contenu de la copie: " << RESET;
+        for (MutantStack<int>::iterator it = msCopy.begin(); it != msCopy.end(); ++it)
+            std::cout << BLUE2 << *it << RESET << " ";
+        std::cout << std::endl;
+    }
+    catch (std::exception &e) {
+        std::cout << RED2 << "Erreur: " << RESET << e.what() << std::endl;
+    }
+    
+    // ---------------- Test 5 : Opérateur d'affectation ----------------
+    std::cout << YELLOW2 << "\n----------------Test 5----------------" << RESET << std::endl;
+    try {
+        MutantStack<int> ms1;
+        ms1.push(7);
+        ms1.push(14);
+        ms1.push(21);
+        MutantStack<int> ms2;
+        ms2 = ms1;  // Affectation
+        std::cout << GREEN2 << "Contenu de ms2 après affectation: " << RESET;
+        for (MutantStack<int>::iterator it = ms2.begin(); it != ms2.end(); ++it)
+            std::cout << BLUE2 << *it << RESET << " ";
+        std::cout << std::endl;
+    }
+    catch (std::exception &e) {
+        std::cout << RED2 << "Erreur: " << RESET << e.what() << std::endl;
+    }
+    
+    // ---------------- Test 6 : Compatibilité avec std::stack ----------------
+    std::cout << YELLOW2 << "\n----------------Test 6----------------" << RESET << std::endl;
+    try {
+        MutantStack<int> ms;
+        ms.push(50);
+        ms.push(60);
+        ms.push(70);
+        std::stack<int> s(ms);  // Copie dans une std::stack
+        std::cout << GREEN2 << "Taille de std::stack copiée: " << BLUE2 << s.size() << RESET << std::endl;
+    }
+    catch (std::exception &e) {
+        std::cout << RED2 << "Erreur: " << RESET << e.what() << std::endl;
+    }
+    
+    return 0;
+}
