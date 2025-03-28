78d9d8ea53d4dca250440e04aa19b68c200e5832
Noralia <sarahgoigoux@gmail.com>
2025-03-29 07:42:47 +0800

    push of /home/noralia/maxime/Desktop/42_Paris/42_Paris_2023/Cursus_2023/Projects/In_progress/cpp/cpp_09/ex02 - update of Sat Mar 29 07:42:47 CST 2025

---
+++ 42_Paris_2023/Cursus_2023/Projects/In_progress/cpp/cpp_09/ex02/main.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mechard <mechard@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/29 07:23:02 by mechard           #+#    #+#             */
+/*   Updated: 2025/03/29 07:23:02 by mechard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+
+bool isPositiveNumber(const std::string &s) {
+    for (size_t i = 0; i < s.size(); ++i) {
+        if (!std::isdigit(s[i]))
+            return false;
+    }
+    return true;
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        std::cerr << "Error" << std::endl;
+        return 1;
+    }
+    PmergeMe pm;
+    for (int i = 1; i < argc; ++i) {
+        std::string token = argv[i];
+        if (!isPositiveNumber(token)) {
+            std::cerr << "Error" << std::endl;
+            return 1;
+        }
+        if (!pm.addNumber(token)) {
+            std::cerr << "Error" << std::endl;
+            return 1;
+        }
+    }
+    pm.printBefore();
+    double timeVector = pm.sortVector();
+    double timeDeque = pm.sortDeque();
+    pm.printAfterVector();
+    std::cout << "Time to process a range of " << argc - 1 << " elements with std::vector : " << timeVector << " us" << std::endl;
+    std::cout << "Time to process a range of " << argc - 1 << " elements with std::deque : " << timeDeque << " us" << std::endl;
+
+    return 0;
+}
