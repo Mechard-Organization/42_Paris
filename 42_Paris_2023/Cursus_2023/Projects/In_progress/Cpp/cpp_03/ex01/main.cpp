1e1f1eacd08aabbcace49d56cde8cc41624cf6a5
Maxime Echard <mechard@bess-f2r1s11.clusters.42paris.fr>
2025-04-28 13:20:47 +0200

    Mon Apr 28 01:20:47 PM CEST 2025 - /home/mechard/Desktop/42_Paris/42_Paris_2023/Cursus_2023/Projects/In_progress update

--- 42_Paris_2023/Cursus_2023/Projects/In_progress/Cpp/cpp_03/ex01/main.cpp
+++ 42_Paris_2023/Cursus_2023/Projects/In_progress/Cpp/cpp_03/ex01/main.cpp
@@ -14,17 +14,29 @@
 
 int main()
 {
+    std::cout << "\033[36m";
     std::cout << "\n--- Test avec ClapTrap ---" << std::endl;
     ClapTrap clap("Clappy");
     clap.attack("Cible1");
     clap.takeDamage(3);
     clap.beRepaired(2);
+    std::cout << "\033[0m";
 
     std::cout << "\n--- Test avec ScavTrap ---" << std::endl;
     ScavTrap scav("Scavy");
+    std::cout << "\033[32m";
     scav.attack("Cible2");
     scav.takeDamage(20);
     scav.beRepaired(10);
+    scav.guardGate();
+    std::cout << "\033[33m";
+    scav.takeDamage(50);
+    scav.beRepaired(10);
+    scav.takeDamage(50);
+    std::cout << "\033[31m";
+    scav.beRepaired(10);
+    scav.takeDamage(50);
+    scav.attack("Cible2");
     scav.guardGate();
 
     return 0;
