f5e28b33dd8b63aa4178a41c1e48ca63d49e9fa4
Maxime Echard <mechard@f2r4s18.clusters.42paris.fr>
2025-06-16 13:39:20 +0200

    Mon Jun 16 01:39:20 PM CEST 2025 - /home/mechard/Downloads/42_paris/42_Paris_2023/Cursus_2023 update

--- add_git42.sh
+++ add_git42.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-echo "$(pwd)"
-if [ "$(pwd)" = "42_Paris/42_Paris_2023/Cursus_2023" ]; then
-	echo test
+if [ "$(pwd | grep "42_paris")" ] && [ ! "$(pwd | grep "Cursus_2023")" ]; then
+	echo "42_paris"
+	pwd=$(pwd)
+	cd ~/Downloads/42_paris
+	git add -A && git commit -m "$(date) - $(pwd) update" && git push
+	cd ~/Downloads/42_paris/42_Paris_2023/Cursus_2023
+	git add -A && git commit -m "$(date) - $(pwd) update" && git push
+elif [ "$(pwd | grep "42_paris")" ] && [ "$(pwd | grep "Cursus_2023")" ]; then
+	echo "Cursus_2023"
 fi
