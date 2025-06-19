bcf930b21bce3cccf021b22b956a05b0527db3f7
Maxime Echard <mechard@f1r9s1.clusters.42paris.fr>
2025-06-19 14:42:43 +0200

    Thu Jun 19 02:42:43 PM CEST 2025 - /home/mechard/Downloads/42_paris update

--- Others/École_42/add_git42.sh
+++ Others/École_42/add_git42.sh
@@ -3,38 +3,37 @@
 pwd=$(pwd)
 
 if [ "$1" = "-g" ] || [ ! "$1" ]; then
-	echo "-g"
 	if [ "$(pwd | grep "42_paris")" ] && [ ! "$(pwd | grep "Cursus_2023")" ]; then
 		echo "Push de 42_Paris et ces sous-dossiers en cours !"
 
 		# Push de 42_paris
 		
 		echo "--------------------------------------------------------------------------------------------------------"
-		echo "Push de 42_Paris en cours."
+		echo -n "Push de 42_Paris en cours."
 		cd ~/Downloads/42_paris
-		echo "\033[1A\033[2KPush de 42_Paris en cours.."
+		echo -n "\033[1A\033[2KPush de 42_Paris en cours.."
 		git add -A && git commit -m "$(date) - $(pwd) update" && git push
-		echo "\033[1A\033[2KPush de 42_Paris en cours..."
+		echo -n "\033[1A\033[2KPush de 42_Paris en cours..."
 		echo "\033[1A\033[2K42_Paris pusher !"
 
 		# Push de Cursus_2023
 		
 		echo "--------------------------------------------------------------------------------------------------------"
-		echo "Push de Cursus_2023 en cours."
+		echo -n "Push de Cursus_2023 en cours."
 		cd ~/Downloads/42_paris/42_Paris_2023/Cursus_2023
-		echo "\033[1A\033[2KPush de Cursus_2023 en cours.."
+		echo -n "\033[1A\033[2KPush de Cursus_2023 en cours.."
 		git add -A && git commit -m "$(date) - $(pwd) update" && git push
-		echo "\033[1A\033[2KPush de Cursus_2023 en cours..."
+		echo -n "\033[1A\033[2KPush de Cursus_2023 en cours..."
 		echo "\033[1A\033[2KCursus_2023 pusher !"
 
 		# Push de Others
 		
 		echo "--------------------------------------------------------------------------------------------------------"
-		echo "Push de Others en cours."
+		echo -n "Push de Others en cours."
 		cd ~/Downloads/42_paris/42_Paris_2023/Others
-		echo "\033[1A\033[2KPush de Others en cours.."
+		echo -n "\033[1A\033[2KPush de Others en cours.."
 		git add -A && git commit -m "$(date) - $(pwd) update" && git push
-		echo "\033[1A\033[2KPush de Others en cours..."
+		echo -n "\033[1A\033[2KPush de Others en cours..."
 		echo "\033[1A\033[2KOther pusher !"
 		echo "--------------------------------------------------------------------------------------------------------"
 
