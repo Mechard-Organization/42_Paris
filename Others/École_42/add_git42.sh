4a9c2b6e0fc769418afc94ef96ea68ef61168548
Maxime Echard <mechard@f1r9s1.clusters.42paris.fr>
2025-06-19 15:25:50 +0200

    Thu Jun 19 03:25:50 PM CEST 2025 - /home/mechard/Downloads/42_paris update

--- Others/École_42/add_git42.sh
+++ Others/École_42/add_git42.sh
@@ -132,19 +132,83 @@ if [ "$1" = "-g" ] || [ ! "$1" ]; then
 		echo "This command can only be used in 42_Paris and 42_Paris's repositories"
 	fi
 elif [ "$1" = "-p" ]; then
-	echo "-p"
 	if [ "$(pwd | grep "42_paris")" ] && [ ! "$(pwd | grep "Cursus_2023")" ]; then
-		echo "42_paris"
+		echo "Push of 42_Paris folder and subfolders in progress !"
+
+		# Push de 42_paris
+		
+		echo "--------------------------------------------------------------------------------------------------------"
 		cd ~/Downloads/42_paris
-		# git add -A && git commit -m "$(date) - $(pwd) update" && git push
+		echo "42_Paris"
+		git pull > /dev/null 2>&1
+		if [ "$?" = "1" ]; then
+			echo "\033[31mAn error as occured !\033[0m"
+		else
+			echo "\033[32mOthers was pulled !\033[0m"
+		fi
+
+		# Push de Cursus_2023
+		
+		echo "--------------------------------------------------------------------------------------------------------"
 		cd ~/Downloads/42_paris/42_Paris_2023/Cursus_2023
-		# git add -A && git commit -m "$(date) - $(pwd) update" && git push
-		cd $pwd
-	elif [ "$(pwd | grep "42_paris")" ] && [ "$(pwd | grep "Cursus_2023")" ]; then
 		echo "Cursus_2023"
-		cd ~/Downloads/42_paris/42_Paris_2023/Cursus_2023
-		# git add -A && git commit -m "$(date) - $(pwd) update" && git push
+		git pull > /dev/null 2>&1
+		if [ "$?" = "1" ]; then
+			echo "\033[31mAn error as occured !\033[0m"
+		else
+			echo "\033[32mOthers was pulled !\033[0m"
+		fi
+
+		# Push de Others
+		
+		echo "--------------------------------------------------------------------------------------------------------"
+		cd ~/Downloads/42_paris/Others
+		echo "Others"
+		git pull > /dev/null 2>&1
+		if [ "$?" = "1" ]; then
+			echo "\033[31mAn error as occured !\033[0m"
+		else
+			echo "\033[32mOthers was pulled !\033[0m"
+		fi
+		echo "--------------------------------------------------------------------------------------------------------"
+
 		cd $pwd
+
+	elif [ "$(pwd | grep "42_paris")" ] && [ "$(pwd | grep "Cursus_2023")" ]; then
+		echo "Push de Cursus_2023 !"
+
+		# Push de Cursus_2023
+		
+		echo "--------------------------------------------------------------------------------------------------------"
+		cd ~/Downloads/42_paris/42_Paris_2023/Cursus_2023
+		echo "Cursus_2023"
+		git pull > /dev/null 2>&1
+		if [ "$?" = "1" ]; then
+			echo "\033[31mAn error as occured !\033[0m"
+		else
+			echo "\033[32mOthers was pulled !\033[0m"
+		fi
+
+	elif [ "$(pwd | grep "42_paris")" ] && [ "$(pwd | grep "Others")" ]; then
+		echo "Push de Others !"
+
+		# Push de Others
+		
+		echo "--------------------------------------------------------------------------------------------------------"
+		cd ~/Downloads/42_paris/Others
+		echo "Others"
+		git pull > /dev/null 2>&1
+		if [ "$?" = "1" ]; then
+			echo "\033[31mAn error as occured !\033[0m"
+		else
+			echo "\033[32mOthers was pulled !\033[0m"
+		fi
+		echo "--------------------------------------------------------------------------------------------------------"
+
+		cd $pwd
+
+	else
+		echo "This command can only be used in 42_Paris and 42_Paris's repositories"
 	fi
 else
 	echo "Usage: 42git { -g | -p }\n"
