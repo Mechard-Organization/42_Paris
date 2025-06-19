a47d41a057cdc438308219a2ba65ca9cdc449118
Maxime Echard <mechard@f1r9s1.clusters.42paris.fr>
2025-06-19 14:48:06 +0200

    Thu Jun 19 02:48:06 PM CEST 2025 - /home/mechard/Downloads/42_paris update

--- Others/École_42/add_git42.sh
+++ Others/École_42/add_git42.sh
@@ -11,30 +11,30 @@ if [ "$1" = "-g" ] || [ ! "$1" ]; then
 		echo "--------------------------------------------------------------------------------------------------------"
 		echo -n "Push de 42_Paris en cours."
 		cd ~/Downloads/42_paris
-		echo -n "\033[1A\033[2KPush de 42_Paris en cours.."
+		echo -en "\033[1A\033[2KPush de 42_Paris en cours.."
 		git add -A > /dev/null 2>&1 && git commit -m "$(date) - $(pwd) update" > /dev/null 2>&1 && git push > /dev/null 2>&1
-		echo -n "\033[1A\033[2KPush de 42_Paris en cours..."
-		echo "\033[1A\033[2K42_Paris pusher !"
+		echo -en "\033[1A\033[2KPush de 42_Paris en cours..."
+		echo -e "\033[1A\033[2K42_Paris pusher !"
 
 		# Push de Cursus_2023
 		
 		echo "--------------------------------------------------------------------------------------------------------"
 		echo -n "Push de Cursus_2023 en cours."
 		cd ~/Downloads/42_paris/42_Paris_2023/Cursus_2023
-		echo -n "\033[1A\033[2KPush de Cursus_2023 en cours.."
+		echo -en "\033[1A\033[2KPush de Cursus_2023 en cours.."
 		git add -A > /dev/null 2>&1 && git commit -m "$(date) - $(pwd) update" > /dev/null 2>&1 && git push > /dev/null 2>&1
-		echo -n "\033[1A\033[2KPush de Cursus_2023 en cours..."
-		echo "\033[1A\033[2KCursus_2023 pusher !"
+		echo -en "\033[1A\033[2KPush de Cursus_2023 en cours..."
+		echo -e "\033[1A\033[2KCursus_2023 pusher !"
 
 		# Push de Others
 		
 		echo "--------------------------------------------------------------------------------------------------------"
 		echo -n "Push de Others en cours."
 		cd ~/Downloads/42_paris/42_Paris_2023/Others
-		echo -n "\033[1A\033[2KPush de Others en cours.."
+		echo -en "\033[1A\033[2KPush de Others en cours.."
 		git add -A > /dev/null 2>&1 && git commit -m "$(date) - $(pwd) update" > /dev/null 2>&1 && git push > /dev/null 2>&1
-		echo -n "\033[1A\033[2KPush de Others en cours..."
-		echo "\033[1A\033[2KOther pusher !"
+		echo -en "\033[1A\033[2KPush de Others en cours..."
+		echo -e "\033[1A\033[2KOther pusher !"
 		echo "--------------------------------------------------------------------------------------------------------"
 
 		cd $pwd
