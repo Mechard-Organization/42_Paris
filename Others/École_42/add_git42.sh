e8cc582c66c0be567db43c47e7c015820225d7e4
Maxime Echard <mechard@f1r9s1.clusters.42paris.fr>
2025-06-19 14:45:19 +0200

    Thu Jun 19 02:45:19 PM CEST 2025 - /home/mechard/Downloads/42_paris update

--- Others/École_42/add_git42.sh
+++ Others/École_42/add_git42.sh
@@ -12,7 +12,7 @@ if [ "$1" = "-g" ] || [ ! "$1" ]; then
 		echo -en "Push de 42_Paris en cours."
 		cd ~/Downloads/42_paris
 		echo -en "\033[1A\033[2KPush de 42_Paris en cours.."
-		git add -A && git commit -m "$(date) - $(pwd) update" && git push
+		git add -A > /dev/null 2>&1 && git commit -m "$(date) - $(pwd) update" > /dev/null 2>&1 && git push > /dev/null 2>&1
 		echo -en "\033[1A\033[2KPush de 42_Paris en cours..."
 		echo "\033[1A\033[2K42_Paris pusher !"
 
@@ -22,7 +22,7 @@ if [ "$1" = "-g" ] || [ ! "$1" ]; then
 		echo -en "Push de Cursus_2023 en cours."
 		cd ~/Downloads/42_paris/42_Paris_2023/Cursus_2023
 		echo -en "\033[1A\033[2KPush de Cursus_2023 en cours.."
-		git add -A && git commit -m "$(date) - $(pwd) update" && git push
+		git add -A > /dev/null 2>&1 && git commit -m "$(date) - $(pwd) update" > /dev/null 2>&1 && git push > /dev/null 2>&1
 		echo -en "\033[1A\033[2KPush de Cursus_2023 en cours..."
 		echo "\033[1A\033[2KCursus_2023 pusher !"
 
@@ -32,7 +32,7 @@ if [ "$1" = "-g" ] || [ ! "$1" ]; then
 		echo -en "Push de Others en cours."
 		cd ~/Downloads/42_paris/42_Paris_2023/Others
 		echo -en "\033[1A\033[2KPush de Others en cours.."
-		git add -A && git commit -m "$(date) - $(pwd) update" && git push
+		git add -A > /dev/null 2>&1 && git commit -m "$(date) - $(pwd) update" > /dev/null 2>&1 && git push > /dev/null 2>&1
 		echo -en "\033[1A\033[2KPush de Others en cours..."
 		echo "\033[1A\033[2KOther pusher !"
 		echo "--------------------------------------------------------------------------------------------------------"
