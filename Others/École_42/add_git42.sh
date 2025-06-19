c5f068ac18638bffee4e8e2205a577b13cbd3317
Maxime Echard <mechard@f1r9s1.clusters.42paris.fr>
2025-06-19 15:08:58 +0200

    Thu Jun 19 03:08:58 PM CEST 2025 - /home/mechard/Downloads/42_paris update

--- Others/École_42/add_git42.sh
+++ Others/École_42/add_git42.sh
@@ -11,8 +11,8 @@ if [ "$1" = "-g" ] || [ ! "$1" ]; then
 		echo "--------------------------------------------------------------------------------------------------------"
 		cd ~/Downloads/42_paris
 		git add -A > /dev/null 2>&1 && git commit -m "$(date) - $(pwd) update" > /dev/null 2>&1
+		git push > /dev/null 2>&1
 		if [ "$?" = "0" ]; then
-			git push > /dev/null 2>&1
 			echo "\033[32m42_Paris pusher !\033[0m"
 		else
 			echo "\033[31mAn error as occured !\033[0m"
@@ -24,8 +24,8 @@ if [ "$1" = "-g" ] || [ ! "$1" ]; then
 		cd ~/Downloads/42_paris/42_Paris_2023/Cursus_2023
 		pwd
 		git add -A > /dev/null 2>&1 && git commit -m "$(date) - $(pwd) update" > /dev/null 2>&1 && git push > /dev/null 2>&1
+		git push > /dev/null 2>&1
 		if [ "$?" = "0" ]; then
-			git push > /dev/null 2>&1
 			echo "\033[32mCursus_2023 pusher !\033[0m"
 		else
 			echo "\033[31mAn error as occured !\033[0m"
@@ -37,8 +37,8 @@ if [ "$1" = "-g" ] || [ ! "$1" ]; then
 		cd ~/Downloads/42_paris/Others
 		pwd
 		git add -A > /dev/null 2>&1 && git commit -m "$(date) - $(pwd) update" > /dev/null 2>&1 && git push > /dev/null 2>&1
+		git push > /dev/null 2>&1
 		if [ "$?" = "0" ]; then
-			git push > /dev/null 2>&1
 			echo "\033[32mOther pusher !\033[0m"
 		else
 			echo "\033[31mAn error as occured !\033[0m"
